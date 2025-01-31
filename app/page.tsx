fc70324a07d3cd35d584b3efe626d4f364c49c06
Natt Nguyen <natt.lefevre@gmail.com>
2025-01-30 18:09:11 -0800

    sign in example

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,33 @@
 "use client"
 
 import Button from "@/components/ui/button"
-
+import { authClient } from "@/utils/auth/auth-client"
 export default function RootPage() {
+  async function signUp() {
+    await authClient.signUp.email(
+      {
+        email: "test@example.com",
+        name: "First Last",
+        password: "12345678",
+      },
+      {
+        onError: (context) => {
+          alert(context.error.message)
+        },
+        onRequest: () => {
+          // show loading
+        },
+        onSuccess: () => {
+          // redirect to the dashboard
+        },
+      },
+    )
+  }
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-      <Button>Click me</Button>
-      <Button variant="secondary">Click me</Button>
-      <Button variant="ghost">Click me</Button>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold underline">Sign up</h1>
+      <Button onClick={signUp}>Click me</Button>
     </div>
   )
 }
