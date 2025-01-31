b02819428781fff7fe93255ef7242f68b9e1c571
Natt Nguyen <natt.lefevre@gmail.com>
2025-01-30 17:50:13 -0800

    install drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,18 @@
+// ====================================================== /
+// :: Notes ::
+// pnpm run db:generate
+// pnpm run db:migrate
+// ====================================================== /
+
+import { defineConfig } from "drizzle-kit"
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("`DATABASE_URL` environment variable is required.")
+}
+
+export default defineConfig({
+  dbCredentials: { url: process.env.DATABASE_URL },
+  dialect: "postgresql",
+  out: "./libs/db/migrations",
+  schema: "./libs/db/schema.ts",
+})
