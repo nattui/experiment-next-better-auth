a69f54cd5732b2a2d1a244847108b2310c5bc9e4
Natt Nguyen <natt.lefevre@gmail.com>
2025-01-30 17:19:59 -0800

    setup

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata, Viewport } from "next"
+import type { PropsWithChildren } from "react"
+import "@/styles/global.css"
+import { Geist, Geist_Mono } from "next/font/google"
+
+const geistSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+})
+
+const geistMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-geist-mono",
+})
+
+export const metadata: Metadata = {
+  description: "Experiment Next Kindle",
+  title: "Experiment Next Kindle",
+}
+
+export const viewport: Viewport = {
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  width: "device-width",
+}
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Button from "@/components/ui/button"
+
+export default async function RootPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <Button>Click me</Button>
+      <Button variant="secondary">Click me</Button>
+      <Button variant="ghost">Click me</Button>
+    </div>
+  )
+}
